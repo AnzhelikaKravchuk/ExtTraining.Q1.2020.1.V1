3941a4c9ebada4f2063aa07d134f5a29b8d54478
AnzhelikaKravchuk <anzhelika.kravchuk@gmail.com>
2020-02-05 19:40:15 +0300

    Initial commit

---
+++ Algorithms.V1/GcdImplementations/EuclideanAlgorithm.cs
@@ -0,0 +1,13 @@
+using System;
+using Algorithms.V1.Interfaces;
+
+namespace Algorithms.V1.GcdImplementations
+{
+    internal class EuclideanAlgorithm : Algorithm
+    {
+        protected override int Func(int first, int second)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
